798fb5adb10807e9469c6a65ebc793f41b037ce3
Brian Bassett <brianbassett@Brians-MBP.lan>
2022-03-22 13:10:50 -0400

    added initial styles folders, fixed some styling on login

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,19 +1,17 @@
 import "./Login.scss";
-import beeIcon from "../../assets/icons/swarm.png";
 
 function Login() {
   return (
     <div className="login">
       <div className="login__header">
         <h1 className="login__title">MONEYBEE</h1>
-        {/* <img className="login__icon" src={beeIcon} /> */}
       </div>
       <form className="login__form">
         <label className="login__label">Username</label>
         <input placeholder="Username" className="login__input" />
         <label className="login__label">Password</label>
         <input placeholder="Password" className="login__input" />
-        <button className="login__button">Login</button>
+        <button className="login__button">Log In</button>
       </form>
     </div>
   );
