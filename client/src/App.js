662643ea434374e57598f62f9ccd1e52f1589e5d
Brian Bassett <brianbassett@Brians-MBP.lan>
2022-03-22 12:50:10 -0400

    created login component, added gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app">
+      <Login />
     </div>
   );
 }
